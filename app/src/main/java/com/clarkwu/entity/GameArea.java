55c3d5067cb068acbd8d56ac50dba7918c7f4b74
jimmyclark <jimmyclark@163.com>
2016-04-10 23:23:57 +0800

    添加rect

--- app/src/main/java/com/clarkwu/entity/GameArea.java
+++ app/src/main/java/com/clarkwu/entity/GameArea.java
@@ -23,6 +23,8 @@ public class GameArea {
     private int[] gameBgNames;
     private Brick[] bricks;
     private Bitmap gameRoomBg;
+    private Bitmap rectBg;
+    private float rectW,rectH;
 
     private MyBrick myBrick;
     private MyBall myBall;
@@ -47,25 +49,95 @@ public class GameArea {
         gameRoomBgW = (float)((float)gameRoomBgW * (float)ConstantValue.SCREEN_DENSITY);
         gameRoomBgH = (float)((float)gameRoomBgH * (float)ConstantValue.SCREEN_DENSITY);
 
+        rectBg = BitmapFactory.decodeResource(ConstantValue.main.getResources(),R.mipmap.rect1201750);
+        rectW = ((float)rectBg.getWidth() / (float)ConstantValue.DEFAULT_DENSITY) ;
+        rectH = (float)rectBg.getHeight() / (float)ConstantValue.DEFAULT_DENSITY;
+
+        rectW = (float)((float)rectW * (float)ConstantValue.SCREEN_DENSITY);
+        rectH = (float)((float)rectH * (float)ConstantValue.SCREEN_DENSITY);
+
         myBrick = new MyBrick();
         myBrick.init();
 
         myBall = new MyBall();
         myBall.init();
 
+        int currentSplitW = 7;
         bricks = new Brick[]{
-                new Brick((gameRoomBgW - 11 * 4 - 5*2)/11 * 0 + 5,ConstantValue.SCREEN_HEIGHT / 35 * 5, Color.rgb(189, 190, 199)),
-            new Brick((gameRoomBgW - 11 * 4-5*2)+4,ConstantValue.SCREEN_HEIGHT / 35 * 5, Color.rgb(189, 190, 199)),
-                new Brick((gameRoomBgW - 11 * 4-5*2)/11*2+4,ConstantValue.SCREEN_HEIGHT / 35 * 5, Color.rgb(189, 190, 199)),
-                new Brick((gameRoomBgW - 11 * 4-5*2)/11*3+4,ConstantValue.SCREEN_HEIGHT / 35 * 5, Color.rgb(189, 190, 199)),
-                new Brick((gameRoomBgW - 11 * 4-5*2)/11*4+4,ConstantValue.SCREEN_HEIGHT / 35 * 5, Color.rgb(189, 190, 199)),
-                new Brick((gameRoomBgW - 11 * 4-5*2)/11*5+4,ConstantValue.SCREEN_HEIGHT / 35 * 5, Color.rgb(189, 190, 199)),
-                new Brick((gameRoomBgW - 11 * 4-5*2)/11*6+4,ConstantValue.SCREEN_HEIGHT / 35 * 5, Color.rgb(189, 190, 199)),
-                new Brick((gameRoomBgW - 11 * 4-5*2)/11*7+4,ConstantValue.SCREEN_HEIGHT / 35 * 5, Color.rgb(189, 190, 199)),
-                new Brick((gameRoomBgW - 11 * 4-5*2)/11*8+4,ConstantValue.SCREEN_HEIGHT / 35 * 5, Color.rgb(189, 190, 199)),
-                new Brick((gameRoomBgW - 11 * 4-5*2)/11*9+4,ConstantValue.SCREEN_HEIGHT / 35 * 5, Color.rgb(189, 190, 199)),
-                new Brick((gameRoomBgW - 11 * 4-5*2)/11*10+4,ConstantValue.SCREEN_HEIGHT / 35 * 5, Color.rgb(189, 190, 199))
+              new Brick(0+rectW/2+4,ConstantValue.SCREEN_HEIGHT/40*7,R.mipmap.rect193195197),
+              new Brick(rectW/2+4+rectW*1+currentSplitW,ConstantValue.SCREEN_HEIGHT/40*7,R.mipmap.rect193195197),
+              new Brick(rectW/2+4+rectW*2+currentSplitW*2,ConstantValue.SCREEN_HEIGHT/40*7,R.mipmap.rect193195197),
+              new Brick(rectW/2+4+rectW*3+currentSplitW*3,ConstantValue.SCREEN_HEIGHT/40*7,R.mipmap.rect193195197),
+              new Brick(rectW/2+4+rectW*4+currentSplitW*4,ConstantValue.SCREEN_HEIGHT/40*7,R.mipmap.rect193195197),
+              new Brick(rectW/2+4+rectW*5+currentSplitW*5,ConstantValue.SCREEN_HEIGHT/40*7,R.mipmap.rect193195197),
+              new Brick(rectW/2+4+rectW*6+currentSplitW*6,ConstantValue.SCREEN_HEIGHT/40*7,R.mipmap.rect193195197),
+              new Brick(rectW/2+4+rectW*7+currentSplitW*7,ConstantValue.SCREEN_HEIGHT/40*7,R.mipmap.rect193195197),
+              new Brick(rectW/2+4+rectW*8+currentSplitW*8,ConstantValue.SCREEN_HEIGHT/40*7,R.mipmap.rect193195197),
+              new Brick(rectW/2+4+rectW*9+currentSplitW*9,ConstantValue.SCREEN_HEIGHT/40*7,R.mipmap.rect193195197),
+              new Brick(rectW/2+4+rectW*10+currentSplitW*10,ConstantValue.SCREEN_HEIGHT/40*7,R.mipmap.rect193195197),
+
+                new Brick(0+rectW/2+4,ConstantValue.SCREEN_HEIGHT/40*8,R.mipmap.rect2067557),
+                new Brick(rectW/2+4+rectW*1+currentSplitW,ConstantValue.SCREEN_HEIGHT/40*8,R.mipmap.rect2067557),
+                new Brick(rectW/2+4+rectW*2+currentSplitW*2,ConstantValue.SCREEN_HEIGHT/40*8,R.mipmap.rect2067557),
+                new Brick(rectW/2+4+rectW*3+currentSplitW*3,ConstantValue.SCREEN_HEIGHT/40*8,R.mipmap.rect2067557),
+                new Brick(rectW/2+4+rectW*4+currentSplitW*4,ConstantValue.SCREEN_HEIGHT/40*8,R.mipmap.rect2067557),
+                new Brick(rectW/2+4+rectW*5+currentSplitW*5,ConstantValue.SCREEN_HEIGHT/40*8,R.mipmap.rect2067557),
+                new Brick(rectW/2+4+rectW*6+currentSplitW*6,ConstantValue.SCREEN_HEIGHT/40*8,R.mipmap.rect2067557),
+                new Brick(rectW/2+4+rectW*7+currentSplitW*7,ConstantValue.SCREEN_HEIGHT/40*8,R.mipmap.rect2067557),
+                new Brick(rectW/2+4+rectW*8+currentSplitW*8,ConstantValue.SCREEN_HEIGHT/40*8,R.mipmap.rect2067557),
+                new Brick(rectW/2+4+rectW*9+currentSplitW*9,ConstantValue.SCREEN_HEIGHT/40*8,R.mipmap.rect2067557),
+                new Brick(rectW/2+4+rectW*10+currentSplitW*10,ConstantValue.SCREEN_HEIGHT/40*8,R.mipmap.rect2067557),
+
+                new Brick(0+rectW/2+4,ConstantValue.SCREEN_HEIGHT/40*9,R.mipmap.rect5799225),
+                new Brick(rectW/2+4+rectW*1+currentSplitW,ConstantValue.SCREEN_HEIGHT/40*9,R.mipmap.rect5799225),
+                new Brick(rectW/2+4+rectW*2+currentSplitW*2,ConstantValue.SCREEN_HEIGHT/40*9,R.mipmap.rect5799225),
+                new Brick(rectW/2+4+rectW*3+currentSplitW*3,ConstantValue.SCREEN_HEIGHT/40*9,R.mipmap.rect5799225),
+                new Brick(rectW/2+4+rectW*4+currentSplitW*4,ConstantValue.SCREEN_HEIGHT/40*9,R.mipmap.rect5799225),
+                new Brick(rectW/2+4+rectW*5+currentSplitW*5,ConstantValue.SCREEN_HEIGHT/40*9,R.mipmap.rect5799225),
+                new Brick(rectW/2+4+rectW*6+currentSplitW*6,ConstantValue.SCREEN_HEIGHT/40*9,R.mipmap.rect5799225),
+                new Brick(rectW/2+4+rectW*7+currentSplitW*7,ConstantValue.SCREEN_HEIGHT/40*9,R.mipmap.rect5799225),
+                new Brick(rectW/2+4+rectW*8+currentSplitW*8,ConstantValue.SCREEN_HEIGHT/40*9,R.mipmap.rect5799225),
+                new Brick(rectW/2+4+rectW*9+currentSplitW*9,ConstantValue.SCREEN_HEIGHT/40*9,R.mipmap.rect5799225),
+                new Brick(rectW/2+4+rectW*10+currentSplitW*10,ConstantValue.SCREEN_HEIGHT/40*9,R.mipmap.rect5799225),
+
+                new Brick(0+rectW/2+4,ConstantValue.SCREEN_HEIGHT/40*10,R.mipmap.rect23115755),
+                new Brick(rectW/2+4+rectW*1+currentSplitW,ConstantValue.SCREEN_HEIGHT/40*10,R.mipmap.rect23115755),
+                new Brick(rectW/2+4+rectW*2+currentSplitW*2,ConstantValue.SCREEN_HEIGHT/40*10,R.mipmap.rect23115755),
+                new Brick(rectW/2+4+rectW*3+currentSplitW*3,ConstantValue.SCREEN_HEIGHT/40*10,R.mipmap.rect23115755),
+                new Brick(rectW/2+4+rectW*4+currentSplitW*4,ConstantValue.SCREEN_HEIGHT/40*10,R.mipmap.rect23115755),
+                new Brick(rectW/2+4+rectW*5+currentSplitW*5,ConstantValue.SCREEN_HEIGHT/40*10,R.mipmap.rect23115755),
+                new Brick(rectW/2+4+rectW*6+currentSplitW*6,ConstantValue.SCREEN_HEIGHT/40*10,R.mipmap.rect23115755),
+                new Brick(rectW/2+4+rectW*7+currentSplitW*7,ConstantValue.SCREEN_HEIGHT/40*10,R.mipmap.rect23115755),
+                new Brick(rectW/2+4+rectW*8+currentSplitW*8,ConstantValue.SCREEN_HEIGHT/40*10,R.mipmap.rect23115755),
+                new Brick(rectW/2+4+rectW*9+currentSplitW*9,ConstantValue.SCREEN_HEIGHT/40*10,R.mipmap.rect23115755),
+                new Brick(rectW/2+4+rectW*10+currentSplitW*10,ConstantValue.SCREEN_HEIGHT/40*10,R.mipmap.rect23115755),
+
+                new Brick(0+rectW/2+4,ConstantValue.SCREEN_HEIGHT/40*11,R.mipmap.rect255139194),
+                new Brick(rectW/2+4+rectW*1+currentSplitW,ConstantValue.SCREEN_HEIGHT/40*11,R.mipmap.rect255139194),
+                new Brick(rectW/2+4+rectW*2+currentSplitW*2,ConstantValue.SCREEN_HEIGHT/40*11,R.mipmap.rect255139194),
+                new Brick(rectW/2+4+rectW*3+currentSplitW*3,ConstantValue.SCREEN_HEIGHT/40*11,R.mipmap.rect255139194),
+                new Brick(rectW/2+4+rectW*4+currentSplitW*4,ConstantValue.SCREEN_HEIGHT/40*11,R.mipmap.rect255139194),
+                new Brick(rectW/2+4+rectW*5+currentSplitW*5,ConstantValue.SCREEN_HEIGHT/40*11,R.mipmap.rect255139194),
+                new Brick(rectW/2+4+rectW*6+currentSplitW*6,ConstantValue.SCREEN_HEIGHT/40*11,R.mipmap.rect255139194),
+                new Brick(rectW/2+4+rectW*7+currentSplitW*7,ConstantValue.SCREEN_HEIGHT/40*11,R.mipmap.rect255139194),
+                new Brick(rectW/2+4+rectW*8+currentSplitW*8,ConstantValue.SCREEN_HEIGHT/40*11,R.mipmap.rect255139194),
+                new Brick(rectW/2+4+rectW*9+currentSplitW*9,ConstantValue.SCREEN_HEIGHT/40*11,R.mipmap.rect255139194),
+                new Brick(rectW/2+4+rectW*10+currentSplitW*10,ConstantValue.SCREEN_HEIGHT/40*11,R.mipmap.rect255139194),
+
+                new Brick(0+rectW/2+4,ConstantValue.SCREEN_HEIGHT/40*12,R.mipmap.rect1201750),
+                new Brick(rectW/2+4+rectW*1+currentSplitW,ConstantValue.SCREEN_HEIGHT/40*12,R.mipmap.rect1201750),
+                new Brick(rectW/2+4+rectW*2+currentSplitW*2,ConstantValue.SCREEN_HEIGHT/40*12,R.mipmap.rect1201750),
+                new Brick(rectW/2+4+rectW*3+currentSplitW*3,ConstantValue.SCREEN_HEIGHT/40*12,R.mipmap.rect1201750),
+                new Brick(rectW/2+4+rectW*4+currentSplitW*4,ConstantValue.SCREEN_HEIGHT/40*12,R.mipmap.rect1201750),
+                new Brick(rectW/2+4+rectW*5+currentSplitW*5,ConstantValue.SCREEN_HEIGHT/40*12,R.mipmap.rect1201750),
+                new Brick(rectW/2+4+rectW*6+currentSplitW*6,ConstantValue.SCREEN_HEIGHT/40*12,R.mipmap.rect1201750),
+                new Brick(rectW/2+4+rectW*7+currentSplitW*7,ConstantValue.SCREEN_HEIGHT/40*12,R.mipmap.rect1201750),
+                new Brick(rectW/2+4+rectW*8+currentSplitW*8,ConstantValue.SCREEN_HEIGHT/40*12,R.mipmap.rect1201750),
+                new Brick(rectW/2+4+rectW*9+currentSplitW*9,ConstantValue.SCREEN_HEIGHT/40*12,R.mipmap.rect1201750),
+                new Brick(rectW/2+4+rectW*10+currentSplitW*10,ConstantValue.SCREEN_HEIGHT/40*12,R.mipmap.rect1201750),
+
         };
+
 
     }
 
--- app/src/main/java/com/clarkwu/entity/brick/Brick.java
+++ app/src/main/java/com/clarkwu/entity/brick/Brick.java
@@ -1,8 +1,12 @@
 package com.clarkwu.entity.brick;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.util.Log;
 
 import com.clarkwu.constants.ConstantValue;
@@ -12,22 +16,29 @@ import com.clarkwu.entity.GameArea;
  * Created by ClarkWu on 2016/4/9.
  */
 public class Brick {
-    private float brickx,brickY,brickW,brickH;
-    private int color = Color.WHITE;
-    public Brick(float brickX,float brickY,int color){
-        this.brickx = brickX;
+    private float brickX,brickY,brickW,brickH;
+    private Bitmap rectBitmap ;
+    public Brick(float brickX,float brickY,int resId){
+        this.brickX = brickX;
         this.brickY = brickY;
-        this.color = color;
 
-        brickH = ConstantValue.SCREEN_HEIGHT / 30;
-        brickW = GameArea.getInstance().getGameRoomBgW()/11;
+        rectBitmap = BitmapFactory.decodeResource(ConstantValue.main.getResources(),resId);
+
+        brickW = (float)rectBitmap.getWidth() / (float)ConstantValue.DEFAULT_DENSITY;
+        brickH = (float)rectBitmap.getHeight() / (float) ConstantValue.DEFAULT_DENSITY;
+
+        brickW = (float)((float)brickW * (float)ConstantValue.SCREEN_DENSITY);
+        brickH = (float)((float)brickH * (float)ConstantValue.SCREEN_DENSITY);
+
     }
 
     public void onDraw(Canvas canvas,Paint paint){
-        paint.setColor(color);
+        Rect rect_picRect = new Rect(0,0,rectBitmap.getWidth(),rectBitmap.getHeight());
+        RectF rect_screenRect = new RectF(brickX,brickY,brickX + brickW,brickY + brickH);
 
-        Log.i("a","x:" + brickx + ";bricky" + brickY + ";brickw:" +brickW + ";"+brickH);
-        canvas.drawRect(brickx,brickY,brickW + brickx,brickY + brickH,paint);
+        canvas.drawBitmap(rectBitmap,rect_picRect,rect_screenRect,paint);
+
+//        Log.i("a","x:" + brickX + ";y:" + brickY + ";w:"+brickW);
     }
 
 }
